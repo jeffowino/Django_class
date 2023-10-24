2653447c648519e7d5745c50187598c52c27f9d4
jeffowino55 <ogayageofrey97@gmail.com>
2023-10-24 08:47:02 +0300

    corrected the home view

--- E_learning/urls.py
+++ E_learning/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
-from .views import add_marks, login_view, logout_view, home
+from .views import add_marks, login_view, logout_view
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    #path('', views.home, name='home'),
     path('add_marks/', views.add_marks, name='add_marks'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
--- E_learning/views.py
+++ E_learning/views.py
@@ -3,7 +3,12 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from .forms import StudentForm, AttendanceForm, MarksForm, NoticeForm
 from .models import Student, Attendance, Marks, Notice
-from django.http import HttpResponse
+from django.http import HttpResponse #this must be imported always
+
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from .forms import StudentForm, AttendanceForm, MarksForm, NoticeForm
 
 def add_marks(request):
     if request.method == 'POST':
@@ -17,10 +22,17 @@ def add_marks(request):
     return render(request, 'add_marks.html', {'form': form})
 
 def login_view(request):
-    return HttpResponse('Testing the login page')
-
-def home(request):
-    return HttpResponse('Testing the home page')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Logged in successfully.')
+            return redirect('some_view')  # Replace with the name of the view you want to redirect to
+        else:
+            messages.error(request, 'Invalid username or password.')
+    return render(request, 'login.html')
 
 def logout_view(request):
     logout(request)
